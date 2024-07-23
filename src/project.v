a9d3f0acc4192a9b0762d3c0f9e3938d042f97f2
eehit.mukherjee@gmail.com <eehit.mukherjee@gmail.com>
2024-07-22 20:08:01 -0400

    first commit

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Eehit Mukherjee
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_eemukh_ControlBlock (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,124 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
+  assign uio_oe[3:0] = 0;
+  assign uio_oe[7:4] = 1;
+  // wire [6:0] Mainout:
+
+  //Inputs
+  wire [5:0] OPcode; // Input 6 OPCode instruction bits
+  wire [1:0] F5_4; //Last two funct. instruction bits
+
+  //Outputs
+  wire RegDst;
+  wire ALUSrc;
+  wire MemtoReg;
+  wire RegWrite;
+  wire MemRead;
+  wire MemWrite;
+  wire Branch;
+
+  //6-bit Outputs
+  wire Out1;
+  wire Out2;
+  wire Out3;
+  wire Out4;
+
+  //internal output (not outputed from circuit)
+  wire [1:0] ALUOp;
+
+  //Bidirectional Inputs
+  wire [3:0] F3_0; //Only F6[3:0] are bidirectional inputs
+
+  //Bidirectional Outputs
+  wire Op0;
+  wire Op1;
+  wire Op2;
+  wire Op3;
+
+  //Assignments
+  assign OPcode = ui_in[5:0];
+
+  assign uo_out[6] = RegDst;
+  assign uo_out[5] = ALUSrc;
+  assign uo_out[4] = MemtoReg;
+  assign uo_out[3] = RegWrite;
+  assign uo_out[2] = MemRead;
+  assign uo_out[1] = MemWrite;
+  assign uo_out[0] = Branch;
+  
+  assign uo_out[7] = 0;
+
+  assign uio_in[3:0] = F3_0[3:0];
+  assign uio_out[0] = Op0;
+  assign uio_out[1] = Op1;
+  assign uio_out[2] = Op2;
+  assign uio_out[3] = Op3;
+
+  //Main Control Unit
+
+  //Variables to assign the outputs of the 4 6-input AND gates
+  assign Out1 = ~OPcode[0] & ~OPcode[1] & ~OPcode[2] & ~OPcode[3] & ~OPcode[4] & ~OPcode[5];
+  assign Out2 = OPcode[0] & OPcode[1] & ~OPcode[2] & ~OPcode[3] & ~OPcode[4] & OPcode[5];
+  assign Out3 = OPcode[0] & OPcode[1] & ~OPcode[2] & OPcode[3] & ~OPcode[4] & OPcode[5];
+  assign Out4 = ~OPcode[0] & ~OPcode[1] & OPcode[2] & ~OPcode[3] & ~OPcode[4] & ~OPcode[5];
+
+  assign RegDst = Out1;
+  assign ALUSrc = Out2 | Out3;
+  assign MemtoReg = Out2;
+  assign RegWrite = Out1 | Out2;
+  assign MemRead = Out2;
+  assign MemWrite = Out3;
+  assign Branch = Out4;
+  assign ALUOp[0] = Out4;
+  assign ALUOp[1] = Out1; 
+
+  //ALU Control Unit
+  assign F5_4[1] = 1;
+  assign F5_4[0] = 0;
+
+  assign Op3 = ALUOp[0] & ~ALUOp[0];
+  assign Op2 = ALUOp[0] | (ALUOp[1] & F3_0[1]);
+  assign Op1 = ~ALUOp[1] | ~F3_0[2];
+  assign Op0 = ALUOp[1] & (F3_0[0] | F3_0[3]);          
+
+  //Sequential design
+
+  // always @(posedge clk) begin
+  //   if (rst_n) begin
+  //     uo_out <= 0;
+  //   end
+
+  //   F6[4] <= 0;
+  //   F6[5] <= 1;
+
+  //   case (Opcode)
+  //     6'b000000 begin
+  //       RegDst <= 1;
+  //       ALUOp[1] <= 1;
+  //       RegWrite <= 1;
+  //     end
+  //     6'b100011 begin
+  //       ALUSrc <= 1;
+  //       MemtoReg <= 1;
+  //       RegWrite <= 1;
+  //       MemRead <= 1;
+  //     end
+  //     6'b101011 begin
+  //       ALUSrc <= 1;
+  //       MemWrite <= 1;
+  //     end
+  //     6'b000100 begin
+  //       Branch <= 1;
+  //       ALUOp[0] <=1;
+  //     end
+  //   endcase
+
+  // end
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, 1'b0};
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -26,15 +26,104 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+
+    #R-type (add)
+
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 72
+    dut._log.info(f"uio_out={dut.uio_out.value}")
+    assert (dut.uio_out.value & 0b1111) == 0b0110
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    #__________________________________________________________________________________
+
+    #R-type (subtract)#
+
+    dut.ui_in.value = 0
+    dut.uio_in.value = 2
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 72
+    assert dut.uio_out.value == 6
+
+    #__________________________________________________________________________________
+
+    #R-type (AND)#
+
+    dut.ui_in.value = 0
+    dut.uio_in.value = 4
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 72
+    assert dut.uio_out.value == 0
+
+    #__________________________________________________________________________________
+    
+    #R-type (OR)#
+
+    dut.ui_in.value = 0
+    dut.uio_in.value = 5
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 72
+    assert dut.uio_out.value == 1
+
+    #__________________________________________________________________________________
+
+    #R-type (Set on Less Then)#
+
+    dut.ui_in.value = 0
+    dut.uio_in.value = 10
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 72
+    assert dut.uio_out.value == 7
+
+    #__________________________________________________________________________________
+
+    #Load Word#
+
+    dut.ui_in.value = 35
+    dut.uio_in.value = 2
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 60
+    assert dut.uio_out.value == 2
+
+    #__________________________________________________________________________________
+
+    #Store Word
+    
+    dut.ui_in.value = 43
+    dut.uio_in.value = 2
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 34
+    assert dut.uio_out.value == 2
+
+    #__________________________________________________________________________________
+
+    #Branch Equal
+    
+    dut.ui_in.value = 4
+    dut.uio_in.value = 6
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 1
+    assert dut.uio_out.value == 6
